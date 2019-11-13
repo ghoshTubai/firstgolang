1229f994302eef5a6df2a851a93b775104d72b57
Ankur Ghosh <ankur.cse10gmail.com>
2019-11-12 23:43:31 -0700

    updated

--- app/start.go
+++ app/start.go
@@ -2,12 +2,11 @@ package app
 
 import (
 	"github.com/gorilla/mux"
-	"net/http"
 )
 
 func Start (){
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/hello",greetings).Methods("GET")
 	router.HandleFunc("/hello",greetings).Methods("POST")
-	http.ListenAndServe("0.0.0.0:5555",router)
+	//http.ListenAndServe("0.0.0.0:5555",router)
 }
